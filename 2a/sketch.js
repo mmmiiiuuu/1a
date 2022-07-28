15774f8e18d885822e8c975d2c9421a9be900030
mmmiiiuuu <109843520+mmmiiiuuu@users.noreply.github.com>
2022-07-27 20:10:16 -0700

    Add files via upload

---
+++ 2a/sketch.js
@@ -0,0 +1,30 @@
+let weight = 0;   
+let seed= .17;
+
+function setup() {
+  var canvas = createCanvas(20, 20);
+ 
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent('sketch-holder');
+
+
+} 
+
+
+function draw() {
+      weight = fxrand() * 300;
+      mood = fxrand()/weight;
+ 
+let oogle=frameCount*weight
+
+  background(0,0,0,0);
+  rectMode(CENTER*oogle);
+  translate(weight+weight,width/weight^width/233*weight/8);
+  rotate(oogle);
+  rect(8, 8, 8, 8);
+  
+  let seed = weight * .01;
+  console.log(seed);
+
+}
+
